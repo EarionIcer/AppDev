03435da3904c23424f598565bb86666c1d8a5ece
EarionIcer <earionicer@gmail.com>
2025-10-16 15:09:24 +0800

    project setup

---
+++ bramwellg3/src/main/java/com/appdev/resq/bramwellg3/Bramwellg3Application.java
@@ -0,0 +1,13 @@
+package com.appdev.resq.bramwellg3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bramwellg3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bramwellg3Application.class, args);
+	}
+
+}
---
+++ bramwellg3/src/test/java/com/appdev/resq/bramwellg3/Bramwellg3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appdev.resq.bramwellg3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Bramwellg3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
